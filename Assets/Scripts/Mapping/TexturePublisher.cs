0291b60913ef3e448c44a94f3c61078b36ed025b
Nathan <nathan@vertile.com>
2020-08-21 10:29:25 -0400

    map creator

---
+++ Assets/Scripts/Mapping/TexturePublisher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class TexturePublisher : UnityPublisher<MessageTypes.Sensor.CompressedImage>
+    {
+        // image message
+        public MessageTypes.Sensor.CompressedImage message;
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+            InitializeMessage();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            message.header.Update();
+            Publish(message);
+        }
+
+        private void InitializeMessage()
+        {
+            message = new MessageTypes.Sensor.CompressedImage();
+            message.format = "jpeg";
+        }
+
+    }
+}

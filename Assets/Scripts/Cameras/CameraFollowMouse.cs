b2a8aecf0041299950f861b69b806df0052de7b0
Mohamed Lotfy Hussein <mohamed.lotfy@rutgers.edu>
2020-09-08 16:50:18 +0200

    Add CameraFollowMouse.cs script

---
+++ Assets/Scripts/Cameras/CameraFollowMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowMouse : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
